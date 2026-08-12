e0074068d289a3c0998f3b6e4cb4ac70ac69ca48
sankalpsoni-cyber <sankalpsoni08062006@gmail.com>
2026-08-12 13:10:41 +0530

    Initial commit

---
+++ A705.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class A705 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+         for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print("I hate");
+            } else {
+                System.out.print("I love");
+            }
+            
+            if (i == n) {
+                System.out.print(" it");
+            } else {
+                System.out.print(" that ");
+            }
+        }
+    }
+}
